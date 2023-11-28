52cb9c656bab4516b31352d104942391c4bbd95e
rcrd91 <152304374+rcrd91@users.noreply.github.com>
2023-11-28 16:53:07 +0100

    Add files via upload

---
+++ 1.  LÓGICA/Ejercicio-056/scripts/myscript.js
@@ -0,0 +1,44 @@
+/***************************************************************************************************************
+ *
+ *   Objetivo: Solicitamnos un número entero n positivo. Si n es par, se divide por 2 y su n es
+ *             impar se multiplica por tres y suma uno. El proceso se repite hasta que n tenga el valor de 1.
+ *             Por ejemplo, la secuencia para n=3 será:
+ *
+ *                  3--> 10 --> 5 --> 16 --> 8 --> 4 --> 2 --> 1
+ *
+ *   Entrada : numero entero n entre 1 y 100
+ *
+ *
+ *   Salida  : La secuencia de valores obtenida tal cual figura en el objetivo
+ *
+ *
+ ***************************************************************************************************************/
+
+function pedirDato(msg, tipo) {
+  let centinela;
+  let dato;
+  do {
+    dato = prompt(msg);
+    if (tipo == "cadena") centinela = isNaN(dato) ? true : false;
+    if (tipo == "entero")
+      centinela =
+        !isNaN(dato) && Number.isInteger(parseFloat(dato)) ? true : false;
+    if (tipo == "flotante")
+      centinela =
+        !isNaN(dato) && !Number.isInteger(parseFloat(dato)) ? true : false;
+    if (tipo == "numero") centinela = !isNaN(dato) ? true : false;
+  } while (!centinela);
+  return dato;
+}
+
+// Iterativa
+function Collatz4(n) {
+  let salida = `${n}`;
+  while (n !== 1) {
+    n = n % 2 ? 3 * n + 1 : n / 2;
+    salida = n % 2 ? salida + `-->${3 * n + 1}` : salida + `-->${n / 2}`;
+  }
+  console.log(salida + "-->" + n);
+}
+
+Collatz4(parseInt(pedirDato("n", "entero")));
---
+++ 1.  LÓGICA/Ejercicio-058/scripts/myscript.js
@@ -0,0 +1,52 @@
+/***************************************************************************************************************
+ *
+ *   Objetivo: Cada numero de Perrin se obtiene sumando los numeros de dos y tres
+ *             posiciones anteriores. Los tres primeros son (3,0,2)
+ *
+ *
+ *   Entrada :
+ *
+ *
+ *   Salida  : 3,0,2,3,2,5,5,7
+ *
+ *
+ ***************************************************************************************************************/
+
+function pedirDato(msg, tipo) {
+  let centinela;
+  let dato;
+  do {
+    dato = prompt(msg);
+    if (tipo == "cadena") centinela = isNaN(dato) ? true : false;
+    if (tipo == "entero")
+      centinela =
+        !isNaN(dato) && Number.isInteger(parseFloat(dato)) ? true : false;
+    if (tipo == "flotante")
+      centinela =
+        !isNaN(dato) && !Number.isInteger(parseFloat(dato)) ? true : false;
+    if (tipo == "numero") centinela = !isNaN(dato) ? true : false;
+  } while (!centinela);
+  return dato;
+}
+
+function perrin(n) {
+  switch (n) {
+    case 0:
+      return 3;
+      break;
+    case 1:
+      return 0;
+      break;
+    case 2:
+      return 2;
+      break;
+    default:
+      return perrin(n - 2) + perrin(n - 3);
+  }
+}
+
+let numero;
+do {
+  numero = parseInt(pedirDato("Que número Perrin quieres", "entero"));
+} while (numero < 0);
+console.log(`El numero Perrin ${perrin(numero)}`);
