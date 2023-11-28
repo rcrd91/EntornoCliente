cb05e016e82c31a5fe6c4fc05849bdba2294e927
rcrd91 <152304374+rcrd91@users.noreply.github.com>
2023-11-28 16:49:49 +0100

    Add files via upload

---
+++ 1.  LÓGICA/Ejercicio-001/scripts/myscript.js
@@ -0,0 +1,16 @@
+/***************************************************************************************************************
+*  
+*   Objetivo: Declarar el script mediante un fichero
+*             Mostrar en la consola el mensaje "Hola Mundo !"
+*             Mostrar en el documenho HTML el mensaje "Hola Mundo!"
+*
+*   Entrada : --
+*
+*
+*   Salida  : Hola Mundo !
+*
+*
+***************************************************************************************************************/
+
+console.log("Hola Mundo!")
+document.write("Hello World!")
